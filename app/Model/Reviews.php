f2c44bbd3df7b067b0ce54a8ae0d15c678e0ba54
mero1998 <mero.123.mf@gmail.com>
2019-04-24 08:23:38 +0300

    Relationship created

--- app/Model/Reviews.php
+++ app/Model/Reviews.php
@@ -3,8 +3,11 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Model\Products;
 class Reviews extends Model
 {
-    //
+    public function products(){
+
+        return $this->belongsTo(Products::class);
+    }
 }
